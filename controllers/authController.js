1c23f771f2bee5d21d5fb6ae843be31b3921ffa2
jayesh-jain252 <jayesh64674@gmail.com>
2022-11-23 16:16:33 +0530

    jwt setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,18 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        name: user.name,
+        location: user.location,
+      },
+      token,
+    });
 };
 
 const login = (req, res) => {
